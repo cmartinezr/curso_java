39a520291cfde4225e4695aac2b76927444946ed
Claudio M. <cmartinezr@gmail.com>
2024-10-05 22:32:09 -0300

    Se incorporan al modelo 2 nuevas clases asociadas a los alumnos y profesores

---
+++ src/com/cmc/cl/bo/Alumno.java
@@ -0,0 +1,23 @@
+package com.cmc.cl.bo;
+
+public class Alumno extends Persona{
+	
+	private String codAlumno;
+
+	/**
+	 * @return
+	 */
+	public String getCodAlumno() {
+		return codAlumno;
+	}
+
+	/**
+	 * @param codAlumno
+	 */
+	public void setCodAlumno(String codAlumno) {
+		this.codAlumno = codAlumno;
+	}
+	
+	
+
+}
---
+++ src/com/cmc/cl/bo/Profesor.java
@@ -0,0 +1,17 @@
+package com.cmc.cl.bo;
+
+public class Profesor extends Persona{
+	
+	private String codProfesor;
+
+	public String getCodProfesor() {
+		return codProfesor;
+	}
+
+	public void setCodProfesor(String codProfesor) {
+		this.codProfesor = codProfesor;
+	}
+	
+	
+
+}
