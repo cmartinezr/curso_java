fc3b0633ad8d7b430e8cfb7bab26c8312cf56d46
Claudio M. <cmartinezr@gmail.com>
2024-10-05 22:32:56 -0300

    Se realizan cambios asociados a la clase Persona y manejo de la clase en Main

--- src/com/cmc/cl/bo/Persona.java
+++ src/com/cmc/cl/bo/Persona.java
@@ -2,23 +2,51 @@ package com.cmc.cl.bo;
 
 public class Persona {
 	
+	/**
+	 * Variables de la clase Persona
+	 */
 	private String nombre;
 	private String apellido;
 	private Integer edad;
 	
 	
+	/**
+	 * @return
+	 */
 	public String getNombre() {
 		return nombre;
 	}
+	/**
+	 * @param nombre
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	/**
+	 * @return
+	 */
 	public String getApellido() {
 		return apellido;
 	}
+	/**
+	 * @param apellido
+	 */
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
+	/**
+	 * @return
+	 */
+	public Integer getEdad() {
+		return edad;
+	}
+	/**
+	 * @param edad
+	 */
+	public void setEdad(Integer edad) {
+		this.edad = edad;
+	}
+	
 	
 
 }
--- src/com/cmc/cl/main/Main.java
+++ src/com/cmc/cl/main/Main.java
@@ -1,10 +1,15 @@
 package com.cmc.cl.main;
 
+import com.cmc.cl.bo.Persona;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
+		
+		Persona persona = new Persona();
+		System.out.println(persona.getNombre());
 
 	}
 
